fbde9d51f3097fb59bc0106d10ca830b21a6d0d2
Yuki Izumi <yuki@kivikakk.ee>
2015-10-17 08:56:47 +1100

    Implement XOR.

--- emu.c
+++ emu.c
@@ -15,14 +15,25 @@ int read_file(char const *filename, uint8_t **out, long *len) {
 }
 
 typedef struct {
-    // f: Z, N, H, C, 0, 0, 0, 0
-    uint8_t a, f, b, c, d, e, h, l;
+    uint8_t a;
+    union {
+        uint8_t f;
+        struct {
+            unsigned int f0 : 4;
+            unsigned int fc : 1;
+            unsigned int fh : 1;
+            unsigned int fn : 1;
+            unsigned int fz : 1;
+        };
+    };
+    uint8_t b, c, d, e, h, l;
     uint16_t sp, pc;
 
     uint8_t ff50;
 } cpu_t;
 
 void dump(cpu_t const *cpu) {
+    printf("\n");
     printf("======== CPU DUMP ========\n");
     printf(" A: %02x      F: %02x\n", cpu->a, cpu->f);
     printf(" B: %02x      C: %02x\n", cpu->b, cpu->c);
@@ -31,6 +42,21 @@ void dump(cpu_t const *cpu) {
     printf("SP: %04x   PC: %04x\n", cpu->sp, cpu->pc);
     printf("==========================\n");
     printf("\n");
+}
+
+int REG(cpu_t const *cpu, int s) {
+    switch (s) {
+        case 0x7: return cpu->a;
+        case 0x0: return cpu->b;
+        case 0x1: return cpu->c;
+        case 0x2: return cpu->d;
+        case 0x3: return cpu->e;
+        case 0x4: return cpu->h;
+        case 0x5: return cpu->l;
+        default:
+            fprintf(stderr, "REG got %x\n", s);
+            exit(1);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -50,10 +76,19 @@ int main(int argc, char **argv) {
 
     dump(&cpu);
 
+    // A 111 7
+    // B 000 0
+    // C 001 1
+    // D 010 2
+    // E 011 3
+    // H 100 4
+    // L 101 5
+
     while (1) {
         uint8_t b = rom[cpu.pc++];
 
         if ((b & 0xcf) == 0x01) {
+            // LD dd, nn
             switch ((b >> 4) & 0x3) {
                 case 0x00:
                     cpu.b = rom[cpu.pc++];
@@ -72,6 +107,10 @@ int main(int argc, char **argv) {
                     cpu.sp |= rom[cpu.pc++];
                     break;
             }
+        } else if ((b & 0xf8) == 0xa8) {
+            // XOR r
+            cpu.a = cpu.a ^ REG(&cpu, b & 0x7);
+            cpu.fz = cpu.a == 0;
         } else {
             fprintf(stderr, "unknown opcode: %x\n", b);
             dump(&cpu);
