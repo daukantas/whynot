1966f92830e8118d98a21ee799d1c2f4053ac8fc
Yuki Izumi <yuki@kivikakk.ee>
2015-10-17 08:22:39 +1100

    initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+BIN = ./emu
+BUILD_DIR = obj
+
+CFLAGS = $(shell $(SDL2_CONFIG) --cflags) -g -Wall
+LDFLAGS = $(shell $(SDL2_CONFIG) --libs) -lSDL2main -framework OpenGL
+
+SRCS = $(wildcard *.c)
+OBJS = $(SRCS:%.c=$(BUILD_DIR)/%.o)
+DEPS = $(OBJS:$(BUILD_DIR)/%.o=$(BUILD_DIR)/%.d)
+
+SDL2_CONFIG = /usr/local/bin/sdl2-config
+
+all: $(BIN)
+
+$(BIN): $(OBJS)
+	gcc -o $@ $(LDFLAGS) $^
+
+-include $(DEPS)
+
+$(BUILD_DIR)/%.o: %.c
+	gcc -o $@ -c $(CFLAGS) -MMD $<
+
+clean:
+	-rm $(BIN) $(OBJS) $(DEPS)
---
+++ emu.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int read_file(char const *filename, char **out, long *len) {
+    FILE *f = fopen(filename, "r");
+    fseek(f, 0, SEEK_END);
+    *len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    *out = malloc(*len);
+    fread(*out, 1, *len, f);
+    fclose(f);
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    char *rom, *cart;
+    long romlen, cartlen;
+    read_file("DMG_ROM.bin", &rom, &romlen);
+    read_file("red.gb", &cart, &cartlen);
+
+    if (romlen != 256) {
+        fprintf(stderr, "ROM not 256 bytes; aborting\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+// vim: set sw=4 et:
