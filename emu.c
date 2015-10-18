16f8d8b6ab535b7bff2c38c1942ab2a52c1391e8
Yuki Izumi <yuki@kivikakk.ee>
2015-10-18 17:11:41 +1100

    well this is prettier

--- emu.c
+++ emu.c
@@ -195,14 +195,20 @@ void lcdc_step(cpu_t *cpu, SDL_Window *window, int t) {
         cpu->lcdc_mode = 0;  // hblank
 
         // render scanline
+        
+        int offs = 0x9C00;  // 0x9800
+        offs += ((cpu->lcdc_line + cpu->lcdc_scy) & 0xff) >> 3;
+        int loffs = cpu->lcdc_scx >> 3;
+        int y = (cpu->lcdc_line + cpu->lcdc_scy) & 0x7;
+        int x = cpu->lcdc_scx & 0x7;
 
-        glColor3ubv(palette[3]);
+        glColor3ubv(palette[rand() % 4]);
 
         glBegin(GL_TRIANGLE_STRIP);
-        glVertex2f(0, cpu->lcdc_line - 0.5f);
-        glVertex2f(SCRW, cpu->lcdc_line - 0.5f);
-        glVertex2f(0, cpu->lcdc_line + 0.5f);
-        glVertex2f(SCRW, cpu->lcdc_line + 0.5f);
+        glVertex2i(0, cpu->lcdc_line);
+        glVertex2i(SCRW, cpu->lcdc_line);
+        glVertex2i(0, cpu->lcdc_line + 1);
+        glVertex2i(SCRW, cpu->lcdc_line + 1);
         glEnd();
     }
 }
